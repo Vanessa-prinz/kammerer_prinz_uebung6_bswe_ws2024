2f59688a8ae514b61a33dd55bd744af68a8c1b2e
Vanessa-prinz <2310859008@fh-burgenland.at>
2024-12-29 12:47:59 +0100

    InsertionSortAlgorithm implementiert + Testklasse dazu

--- kammerer_prinz_uebung6_bswe_ws2024/src/main/java/at/fh_burgenland/bswe/algo/SortAlgorithm/InsertionSortAlgorithm.java
+++ kammerer_prinz_uebung6_bswe_ws2024/src/main/java/at/fh_burgenland/bswe/algo/SortAlgorithm/InsertionSortAlgorithm.java
@@ -4,7 +4,22 @@ public class InsertionSortAlgorithm extends SortAlgorithm {
 
     @Override
     public int[] getSortedList(int[] originalList) {
-        //TODO: implement
+        if (originalList == null || originalList.length == 0) {
+            return null;
+        }
+
+        for (int i = 1; i < originalList.length; i++) {
+            int key = originalList[i];
+            int j = i - 1;
+
+            while (j >= 0 && originalList[j] > key) { //Elemente die größer als key sind, werden um eine Position nach rechts verschoben
+                originalList[j + 1] = originalList[j];
+                j = j - 1;
+            }
+
+            originalList[j + 1] = key; //key an richtige Stelle setzen
+        }
+
         return originalList;
     }
 }
--- kammerer_prinz_uebung6_bswe_ws2024/src/test/java/at/fh_burgenland/bswe/algo/InsertionSortAlgorithmTests.java
+++ kammerer_prinz_uebung6_bswe_ws2024/src/test/java/at/fh_burgenland/bswe/algo/InsertionSortAlgorithmTests.java
@@ -1,5 +1,51 @@
 package at.fh_burgenland.bswe.algo;
 
+import at.fh_burgenland.bswe.algo.SortAlgorithm.InsertionSortAlgorithm;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 public class InsertionSortAlgorithmTests {
-    // TODO: leere Liste, ein element liste, sorted liste, unsorted liste, unsorted lange liste
+
+    @Test
+    public void executeTest_EmptyList() {
+        Assertions.assertNull(new InsertionSortAlgorithm().getSortedList(new int[0]));
+    }
+
+    @Test
+    public void executeTest_OneElement() {
+        Assertions.assertTrue(Arrays.equals(
+                TestData.getListSingleElement(),
+                new InsertionSortAlgorithm().getSortedList(TestData.getListSingleElement())));
+    }
+
+    @Test
+    public void executeTest_alreadySortedList() {
+        Assertions.assertTrue(Arrays.equals(
+                TestData.getListSortedLong(),
+                new InsertionSortAlgorithm().getSortedList(TestData.getListSortedLong())));
+    }
+
+    @Test
+    public void executeTest_unsortedList() {
+        Assertions.assertTrue(Arrays.equals(
+                Arrays.stream(TestData.getListUnsortedShort()).sorted().toArray(),
+                new InsertionSortAlgorithm().getSortedList(TestData.getListUnsortedShort())));
+    }
+
+    @Test
+    public void executeTest_unsortedLongList() {
+        Assertions.assertTrue(Arrays.equals(
+                Arrays.stream(TestData.getListUnsortedLong()).sorted().toArray(),
+                new InsertionSortAlgorithm().getSortedList(TestData.getListUnsortedLong())));
+    }
+
+
+    @Test
+    public void executeTest_negativeNumbers() {
+        Assertions.assertTrue(Arrays.equals(
+                Arrays.stream(TestData.getListUnsortedWithNegative()).sorted().toArray(),
+                new InsertionSortAlgorithm().getSortedList(TestData.getListUnsortedWithNegative())));
+    }
 }
